9eaebc42225b5c14b7b41493331dfda31ff9faaa
方又生 <fangyusheng@fangyoushengde-MacBook-Pro.local>
2017-10-17 18:18:10 +0800

    Initial Commit

---
+++ godutch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  godutch
+//
+//  Created by 方又生 on 2017/10/17.
+//  Copyright © 2017年 方又生. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
